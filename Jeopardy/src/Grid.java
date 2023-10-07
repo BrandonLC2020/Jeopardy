d07de4f328e798518012eecb8f3cded36919539f
Brandon Lamer-Connolly <43575529+BrandonLC2020@users.noreply.github.com>
2023-10-07 14:32:49 -0400

    starting up

---
+++ Jeopardy/src/Grid.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+public class Grid extends JPanel {
+
+    public ArrayList
+}
---
+++ Jeopardy/src/Jeopardy.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+public class Jeopardy {
+    public static JFrame game;
+
+}
